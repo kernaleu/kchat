b83f68420588d8b655de873d45d42250e6f85fc4
Siren <siren@kernal.eu>
2020-09-16 20:00:44 +0300

    Small change in our banner

--- config.h
+++ config.h
@@ -6,12 +6,12 @@
 #define MAX_CLIENTS 17
 #define AUTH_FILE "auth.txt"
 #define MOTD  "\r\e[1;34m" \
-"             __\n" \
-"          -=(o '.\n" \
-" \e[3mKernal\e[1m      '.-.\\\n" \
-" \e[3mCommunity\e[1m   /|  \\\\\n" \
-" \e[3m2017-2020\e[1m   '|  ||\n" \
-"             _\\_):,_" \
+"                __\n" \
+"             -=(o '.\n" \
+" \e[3mKernal   \e[1m      '.-.\\\n" \
+" \e[3mCommunity   \e[1m   /|  \\\\\n" \
+" \e[3m2017-present\e[1m   '|  ||\n" \
+"                _\\_):,_" \
 "\e[0m\n"
 
 
