27ab43adc87f05e363105ebd1e0ff2b5756e7af9
Stnby <stnby@kernal.eu>
2020-11-05 09:29:57 +0200

    Update MOTD to something more fun.

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
 #define BUF_IN_SIZE 512
 #define MAX_CLIENTS 17
 #define AUTH_FILE "auth.txt"
+/*
 #define MOTD  "\r\e[1;34m" \
 "                __\n" \
 "             -=(o '.\n" \
@@ -14,6 +15,16 @@
 " \e[3m2017-present\e[1m   '|  ||\n" \
 "                _\\_):,_" \
 "\e[0m\n"
-
+*/
+#define MOTD  "\r\e[1;34m" \
+"                  __\n" \
+"               -=(o '.\n" \
+" \e[3mKernal     \e[1m      '.-.\\\n" \
+" \e[3mCommunity     \e[1m   /|  \\\\\n" \
+" \e[3m2017-ELF��@hV@8  \e[1m'|  ||\n" \
+"                  _\\_):,_\n" \
+" Segmentation fault\n" \
+"             (core dumped)" \
+"\e[0m\n"
 
 #endif
