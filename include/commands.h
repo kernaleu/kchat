e2a42debfd2c32486d2fcb85802ffed78cea9365
Siren <siren@kernal.eu>
2022-01-10 23:53:35 +0200

    Add license notice to the rest of the files

--- include/commands.h
+++ include/commands.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2021 İrem Kuyucu <siren@kernal.eu>
+ * Copyright (C) 2021 Laurynas Četyrkinas <stnby@kernal.eu>
+ *
+ * This file is part of Kernal Chat.
+ *
+ * Kernal Chat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kernal Chat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kernal Chat.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
